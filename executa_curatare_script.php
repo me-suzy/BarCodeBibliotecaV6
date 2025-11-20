14864ef4e2f51b4fe120549388ab130a53eed929
Doar EU <me.suzana@gmail.com>
2025-11-20 23:52:46 +0200

    Add executa_curatare_script.php

---
+++ executa_curatare_script.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Script salvat: executa_curatare.php
+ * Data creare: <?php echo date('Y-m-d H:i:s'); ?>
+ * 
+ * Acest script execută curățarea bazei de date:
+ * 1. Șterge dublurile
+ * 2. Limitează la maxim 6 cărți per utilizator
+ * 
+ * Rulează: php executa_curatare.php
+ */
+
+// Scriptul original este în executa_curatare.php
+

c0e09c8a5a2e4c545b39213afd53336c6990e653
Doar EU <me.suzana@gmail.com>
2025-11-20 23:57:17 +0200

    Add sterge_dubluri_script.php

---
+++ sterge_dubluri_script.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Script salvat: sterge_dubluri_imprumuturi.php
+ * Data creare: <?php echo date('Y-m-d H:i:s'); ?>
+ * 
+ * Acest script șterge dublurile din tabelul imprumuturi
+ * Rulează în browser: http://localhost/sterge_dubluri_imprumuturi.php
+ */
+
+// Scriptul original este în sterge_dubluri_imprumuturi.php
+

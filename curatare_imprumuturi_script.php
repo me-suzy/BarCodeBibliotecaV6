7de6d697bd2e9847e2657a374e488fbd8bf759ae
Doar EU <me.suzana@gmail.com>
2025-11-20 23:51:31 +0200

    Add curatare_imprumuturi_script.php

---
+++ curatare_imprumuturi_script.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Script salvat: curatare_imprumuturi.php
+ * Data creare: <?php echo date('Y-m-d H:i:s'); ?>
+ * 
+ * Acest script curăță baza de date:
+ * 1. Șterge dublurile (păstrează prima înregistrare)
+ * 2. Limitează la maxim 6 cărți per utilizator (păstrează primele 6)
+ * 
+ * Rulează în browser: http://localhost/curatare_imprumuturi.php
+ */
+
+// Scriptul original este în curatare_imprumuturi.php
+

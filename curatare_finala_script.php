4538fffb1b7553dabb3e6bcf87aa1d625ba5cad6
Doar EU <me.suzana@gmail.com>
2025-11-20 23:51:27 +0200

    Add curatare_finala_script.php

---
+++ curatare_finala_script.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Script salvat: curatare_finala.php
+ * Data creare: <?php echo date('Y-m-d H:i:s'); ?>
+ * 
+ * Acest script face curățarea completă:
+ * 1. Actualizează statusul pentru cărțile returnate
+ * 2. Șterge dublurile
+ * 3. Limitează la maxim 6 cărți per utilizator
+ * 
+ * Rulează: php curatare_finala.php
+ */
+
+// Scriptul original este în curatare_finala.php
+

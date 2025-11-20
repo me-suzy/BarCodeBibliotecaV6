a21526ab4b627d29369fdcefc8296bce39c570ba
Doar EU <me.suzana@gmail.com>
2025-11-20 23:58:20 +0200

    Add update_database_script.php

---
+++ update_database_script.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Script salvat: update_database.php
+ * Data creare: <?php echo date('Y-m-d H:i:s'); ?>
+ * 
+ * Acest script actualizează baza de date cu câmpurile necesare pentru sistemul de sesiuni
+ */
+
+// Scriptul original este în update_database.php
+
